43bdf4decc5cdc179ab5438560d449134a1a7dce
keerthimunisgithub <122539244+keerthimunisgithub@users.noreply.github.com>
2023-04-20 13:43:27 +0100

    Updating design error

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -13,7 +13,7 @@ const ProductCard = (props) => {
     let location = useLocation(); 
   return (
  <>
-    <div className={` ${location.pathname ==="/store" ? `gr-${grid}` : "col-3"} `}>
+    <div className={` ${location.pathname ==="/product" ? `gr-${grid}` : "col-3"} `}>
         <Link to=":id" className="product-card position-relative" alt="">
             <div className="wishlist-icon position-absolute">
                 <button className='border-0 bg-transparent'>
@@ -49,7 +49,7 @@ const ProductCard = (props) => {
             </div>
         </Link>
         </div>
-        <div className={` ${location.pathname ==="/store" ? `gr-${grid}` : "col-3"} `}>
+        <div className={` ${location.pathname ==="/product" ? `gr-${grid}` : "col-3"} `}>
         <Link className="product-card position-relative">
             <div className="wishlist-icon position-absolute">
                 <button className='border-0 bg-transparent'>
